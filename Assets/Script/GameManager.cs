14fab0697776e9d987ad2bdb19a2d5da1e101d11
jpfalves <32970867+jpfalves@users.noreply.github.com>
2019-10-24 21:03:45 -0300

    Updated fire assets

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+
+    private static GameManager uniqueInstance = null;
+
+    public static GameManager GetInstance()
+    {
+        return uniqueInstance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Awake()
+    {
+        if (uniqueInstance == null)
+        {
+            uniqueInstance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/GameState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FGameState
+{
+    public enum EState { MainMenu, Map1, GameOver};
+    private EState currentEState = EState.MainMenu;
+    private Fstate _currentState = null;
+
+    public void onUpdate()
+    {
+       EState frameState = _currentState.onUpdate();
+    }
+    
+}
+
+public abstract class Fstate
+{
+    protected Fstate _nextState;
+
+    public abstract void onBegin();
+    public abstract FGameState.EState onUpdate();
+    public abstract void onEND();
+
+}
+
+public class Fmenu : Fstate
+{
+    public override void onBegin()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override FGameState.EState onUpdate()
+    {
+        return FGameState.EState.MainMenu;
+    }
+
+    public override void onEND()
+    {
+        throw new System.NotImplementedException();
+    }
+}
